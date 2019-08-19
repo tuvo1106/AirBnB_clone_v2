06141824a2f39e6bb73de102f48c28fa1eb6a8bd
Tu Vo <tuvo1106@gmail.com>
2019-08-19 19:33:17 +0000

    Update script and ran on both servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -9,7 +9,6 @@ mkdir -p /data/web_static/
 mkdir -p /data/web_static/releases/
 mkdir -p /data/web_static/shared/
 mkdir -p /data/web_static/releases/test/
-touch /data/web_static/releases/test/index.html
 echo "<html>
   <head>
   </head>
@@ -17,7 +16,7 @@ echo "<html>
     Holberton School
   </body>
 </html>" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test /data/web_static/current
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data
 sed -i "/listen 80 default_server/a location /hbnb_static/ {alias /data/web_static/current;}" /etc/nginx/sites-available/default
 service nginx restart

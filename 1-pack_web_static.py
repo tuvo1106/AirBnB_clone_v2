210d2fed61ef544ee43d3bdd3bc5bf4b0fdb2821
Tu Vo <tuvo1106@gmail.com>
2019-08-19 20:01:21 +0000

    Add task 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -11,10 +11,9 @@ n = datetime.now()
 def do_pack():
     """Packs web_static files into .tgz file"""
 
-    # sudo("mkdir -p /versions")
-
     file_name = 'versions/web_static_{}{}{}{}{}{}.tgz'\
                 .format(n.year, n.month, n.day, n.hour, n.minute, n.second)
-    command = local("tar -cvzf " + file_name + " web_static")
+    local('mkdir -p versions')
+    command = local("tar -cvzf " + file_name + " ./web_static/")
     if command.succeeded:
         return file_name

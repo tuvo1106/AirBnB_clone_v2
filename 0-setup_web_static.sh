fc5d55504b920fa888d10e2c8ecc141d7a64bbef
Tu Vo <tuvo1106@gmail.com>
2019-08-19 19:00:05 +0000

    Changed ownership of data

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -17,7 +17,7 @@ echo "<html>
   </body>
 </html>" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test /data/web_static/current
-chown -R ubuntu:ubuntu /data/
+chown -R ubuntu:ubuntu /data
 sed -i "/listen 80 default_server/a location /hbnb_static/ {alias /data/web_static/current;autoindex off;}" /etc/nginx/sites-available/default
 service nginx restart
 exit 0

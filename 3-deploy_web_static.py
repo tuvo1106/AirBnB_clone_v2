c2a20ab00e6b19cac13b149d34570892dad65867
Tu Vo <tuvo1106@gmail.com>
2019-08-19 22:14:08 +0000

    Copy and paste functions from previous assignments in task 3

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -5,10 +5,52 @@ from fabric.api import *
 from os import path
 from datetime import datetime
 
-do_pack = __import__('1-pack_web_static').do_pack
-do_deploy = __import__('2-do_deploy_web_static').do_deploy
+# do_pack = __import__('1-pack_web_static').do_pack
+# do_deploy = __import__('2-do_deploy_web_static').do_deploy
 
 env.hosts = ['34.73.136.156', '35.196.227.92']
+n = datetime.now()
+
+
+def do_pack():
+    """Packs web_static files into .tgz file"""
+
+    file_name = 'versions/web_static_{}{}{}{}{}{}.tgz'\
+                .format(n.year, n.month, n.day, n.hour, n.minute, n.second)
+    local('mkdir -p versions')
+    command = local("tar -cvzf " + file_name + " ./web_static/")
+    if command.succeeded:
+        print("Success.")
+        return file_name
+
+
+def do_deploy(archive_path):
+    """Deploys archive"""
+    if not path.exists(archive_path):
+        return False
+    ret_value = True
+    a = put(archive_path, '/tmp/')
+    if a.failed:
+        ret_value = False
+    arch = archive_path.replace(".tgz", "").replace("versions/", "")
+    b = run('mkdir -p /data/web_static/releases/' + arch + '/')
+    if b.failed:
+        ret_value = False
+    c = run('tar -xzf /tmp/' + arch + '.tgz' +
+            ' -C /data/web_static/releases/' + arch + '/')
+    if c.failed:
+        ret_value = False
+    d = run('rm /tmp/' + arch + '.tgz')
+    if d.failed:
+        ret_value = False
+    e = run('mv /data/web_static/releases/' + arch +
+            '/web_static/* /data/web_static/releases/' + arch + '/')
+    if e.failed:
+        ret_value = False
+    f = run('rm -rf /data/web_static/releases/' + arch + '/web_static')
+    if f.failed:
+        ret_value = False
+    g = run('rm -rf /data/web_static/current')
 
 
 def deploy():

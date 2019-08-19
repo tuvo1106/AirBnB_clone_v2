218bf0d290821ea0e6876ae77f3ef8c5dd238512
Tu Vo <tuvo1106@gmail.com>
2019-08-19 19:40:23 +0000

    Rewrite task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,14 +1,10 @@
 #!/usr/bin/env bash
 # Sets up web servers for the deployment of web_static
 
-apt-get update &&
+apt-get update
 apt-get -y install nginx
 ufw allow 'Nginx HTTP'
-mkdir -p /data/
-mkdir -p /data/web_static/
-mkdir -p /data/web_static/releases/
-mkdir -p /data/web_static/shared/
-mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/{releases/test,shared}
 echo "<html>
   <head>
   </head>
@@ -18,6 +14,6 @@ echo "<html>
 </html>" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data
-sed -i "/listen 80 default_server/a location /hbnb_static/ {alias /data/web_static/current;}" /etc/nginx/sites-available/default
+sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
 service nginx restart
 exit 0

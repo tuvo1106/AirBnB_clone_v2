31a24bb0048146af457e7febc0c335d8aa4306fd
Tu Vo <tuvo1106@gmail.com>
2019-08-19 18:52:29 +0000

    Add exit condition on task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -20,3 +20,4 @@ ln -sf /data/web_static/releases/test /data/web_static/current
 chown -R ubuntu:ubuntu /data/
 sed -i "/listen 80 default_server/a location /hbnb_static/ {\nalias /data/web_static/current;\nautoindex off;}" /etc/nginx/sites-available/default
 service nginx restart
+exit 0

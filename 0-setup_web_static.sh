31a0ea55eba9b95c69b17aeea1111080429c19f4
Tu Vo <tuvo1106@gmail.com>
2019-08-19 19:03:20 +0000

    Remove autoindex from task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -18,6 +18,6 @@ echo "<html>
 </html>" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test /data/web_static/current
 chown -R ubuntu:ubuntu /data
-sed -i "/listen 80 default_server/a location /hbnb_static/ {alias /data/web_static/current;autoindex off;}" /etc/nginx/sites-available/default
+sed -i "/listen 80 default_server/a location /hbnb_static/ {alias /data/web_static/current;}" /etc/nginx/sites-available/default
 service nginx restart
 exit 0

2480939c21618db9361d51e36a67a4355694cb5c
Tu Vo <tuvo1106@gmail.com>
2019-08-20 22:50:56 +0000

    Reorder manifests

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -9,14 +9,6 @@ package { 'nginx':
   require => Exec['update']
 }
 
-file_line { 'hbnb_static':
-  ensure  => 'present',
-  path    => '/etc/nginx/sites-available/default',
-  after   => 'server_name _;',
-  line    => 'location /hbnb_static/ { alias /data/web_static/current/;}',
-  require => Package['nginx'],
-}
-
 file { ['/data/', '/data/web_static/', '/data/web_static/releases/', '/data/web_static/releases/test', '/data/web_static/shared']:
    ensure => 'directory',
     owner => 'ubuntu',
@@ -42,6 +34,14 @@ Holberton School
 </html>",
      owner => 'ubuntu',
      group => 'ubuntu',
+}
+
+file_line { 'hbnb_static':
+  ensure  => 'present',
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'server_name _;',
+  line    => 'location /hbnb_static/ { alias /data/web_static/current/;}',
+  require => Package['nginx'],
 }
 
 service { 'nginx':

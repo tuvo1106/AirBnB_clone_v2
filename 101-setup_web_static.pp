8bc4a8a2ff1a8f32f116bb33f7a6c398aa36d55e
Tu Vo <tuvo1106@gmail.com>
2019-08-22 17:04:52 +0000

    Update task 101 with exec command

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -35,6 +35,10 @@ file { '/data/web_static/current':
    group => 'ubuntu',
 }
 
+exec { 'config':
+  command => '/usr/bin/env sed -i "/server_name _;/a location /hbnb_static/ { alias /data/web_static/current/;}" /etc/nginx/sites-available/default'
+}
+
 service { 'nginx':
   ensure  => running,
   require => Package['nginx'],

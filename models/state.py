8d9df6e0cb49bcc423ef8c39cbd3f9ccbc2a8f93
Tu Vo <tuvo1106@gmail.com>
2019-08-28 06:29:35 +0000

    Add static files

--- models/state.py
+++ models/state.py
@@ -18,18 +18,19 @@ class State(BaseModel, Base):
     name = Column(String(128),
                   nullable=False)
 
-    cities = relationship("City",
-                          backref="state",
-                          cascade="all, delete-orphan")
-
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        @property
-        def cities(self):
-            _list = []
-            for city in self.cities:
-                if city.state_id == self.id:
-                    _list.append(city)
-            return _list
+        cities = relationship("City",
+                              backref="state",
+                              cascade="all, delete-orphan")
+
+    # if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+    #    @property
+    #    def cities(self):
+    #        _list = []
+    #        for city in self.cities:
+    #            if city.state_id == self.id:
+    #                _list.append(city)
+    #        return _list
 
     if os.getenv('HBNB_TYPE_STORAGE') == 'fs':
         @property

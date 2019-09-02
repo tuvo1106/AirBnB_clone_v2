f7382c5e72c3749c346113f730c8ebc6e1b04f7b
Tu Vo <tuvo1106@gmail.com>
2019-09-02 00:10:41 +0000

    Update task 0 and 1

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -10,8 +10,9 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello():
-    """Returns 'Hello HBNB@'"""
+    """Returns 'Hello HBNB!'"""
     return 'Hello HBNB!'
 
 
-app.run(host="0.0.0.0", port="5000")
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="5000")
